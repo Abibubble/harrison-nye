1e22e2c76b14bd62818d5d0e89ac6979cbf4b55f
Abigail Harrison <abigail@giffgaff.co.uk>
2023-09-23 00:24:24 +0100

    wip: Update DayPlan, add example timings

--- src/pages/day-plan.js
+++ src/pages/day-plan.js
@@ -1,31 +1,59 @@
 import styled from 'styled-components'
 import styles from '../styles/styles'
 
+import PageSection from '../components/page-section'
 import Title from '../components/title'
 import Wrapper from '../components/wrapper'
 
 export default function DayPlan() {
   return (
     <Wrapper wedding>
-      <Container>
+      <PageSection>
         <Title>Day Plan</Title>
-        <p>Something will happen</p>
-        <p>At some time</p>
+        <PlanGrid>
+          <p>12pm</p>
+          <p>Ceremony</p>
+          <p>1pm</p>
+          <p>Photos</p>
+          <p>2pm</p>
+          <p>Chill (& lunch)</p>
+          <p>6pm</p>
+          <p>Reception</p>
+          <p>7pm</p>
+          <p>Food</p>
+          <p>8pm</p>
+          <p>Get drunk</p>
+        </PlanGrid>
         <Button href='/welcome'>Back</Button>
-      </Container>
+      </PageSection>
     </Wrapper>
   )
 }
 
-const Container = styled.div`
-  padding: 0 ${styles.spacer.small} ${styles.spacer.small};
+const Button = styled.a`
+  text-decoration: none;
+  font-family: 'Roboto', Arial, Helvetica, sans-serif;
+  margin: ${styles.spacer.tiny};
+  border: 6px solid;
+  border-image-slice: 1;
+  border-width: 4px;
+  border-image-source: linear-gradient(
+    to right,
+    ${styles.colour.darkBlue},
+    ${styles.colour.darkPurple}
+  );
+  background: ${styles.colour.transparentWhite};
+  font-size: 1.4rem;
+  padding: 0 ${styles.spacer.small};
+
+  &:visited {
+    color: ${styles.colour.black};
+  }
 `
 
-const Button = styled.a`
-  margin: ${styles.spacer.tiny};
-  border-radius: ${styles.spacer.tiny};
-  border: ${styles.spacer.fine} solid grey;
-  background-color: ${styles.colour.blue};
-  font-size: 1.5rem;
-  padding: 0 ${styles.spacer.small};
+const PlanGrid = styled.div`
+  margin: ${styles.spacer.medium} auto;
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  gap: ${styles.spacer.tiny};
 `
