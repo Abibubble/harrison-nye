72a8cb10d002650f7bf4e8ce7b2e0c46cbc1fa1a
Abigail Harrison <abigail@giffgaff.co.uk>
2023-10-07 21:27:18 +0100

    wip: Update guest list, style parking page

--- src/pages/parking-accommodation.js
+++ src/pages/parking-accommodation.js
@@ -12,63 +12,65 @@ export default function ParkingAccommodation() {
       <PageSection>
         <Title>Parking & Accommodation</Title>
         <Subheading>Parking</Subheading>
-        <p>
+        <Text>
           Unfortunately there isn't much parking space at The Woodman, and we
           recommend that you don't try to park in their car park. There is a
           small amount of free parking down the nearby side roads.
-        </p>
-        <p>
+        </Text>
+        <Text>
           Alternatively, there is a car park near Eastcote station, and the 282
           bus will take you to directly outside The Woodman.
-        </p>
-        <p>
+        </Text>
+        <Text>
           If you are arriving by public transport, Eastcote station is on the
           Uxbridge branch of the Metropolitan line, followed by the 282 bus to
           take you to The Woodman.
-        </p>
+        </Text>
         <Subheading>Accommodation</Subheading>
-        <p>
+        <Text>
           If you're travelling from further afield (or simply don't fancy a long
           taxi ride home), there are a few hotels nearby:
-        </p>
-        <p>
-          <a
+        </Text>
+        <Text>
+          <Link
             href='https://www.premierinn.com/gb/en/hotels/england/greater-london/london/london-ruislip.html?cid=GLBC_RUIBEE'
             rel='noopener noreferrer'
             target='_blank'
           >
             Premier Inn Ruislip
-          </a>{' '}
-          is next to the Beefeater pub, which you can get to on the 282 bus,
+          </Link>{' '}
+          is next to the Beefeater pub, which you can get to on the H13 bus,
           followed by a small walk.
-        </p>
-        <p>
-          <a
+        </Text>
+        <Text>
+          <Link
             href='https://www.premierinn.com/gb/en/hotels/england/hertfordshire/rickmansworth/rickmansworth.html'
             rel='noopener noreferrer'
             target='_blank'
           >
             Premier Inn Rickmansworth
-          </a>{' '}
+          </Link>{' '}
           is a short taxi ride away.
-        </p>
-        <p>
-          <a
+        </Text>
+        <Text>
+          <Link
             href='https://www.thetudorhotel.com/'
             rel='noopener noreferrer'
             target='_blank'
           >
             The Tudor Hotel
-          </a>{' '}
+          </Link>{' '}
           is approximately a 15 minute walk away.
-        </p>
-        <p>
+        </Text>
+        <Text>
           There are many other hotels in the area, and these are the least
           pricey options in the area. If these three options don't suit you,
-          feel free to take a look for other places. The Woodman's postcode is
-          HA5 2PR, and bear in mind that it will be a Sunday, so some public
-          transport may end early.
-        </p>
+          feel free to take a look for other places.
+        </Text>
+        <Text>
+          The Woodman's postcode is HA5 2PR, and bear in mind that it will be a
+          Sunday, so some public transport may end early.
+        </Text>
         <Button href='/welcome'>Back</Button>
       </PageSection>
     </Wrapper>
@@ -77,4 +79,14 @@ export default function ParkingAccommodation() {
 
 const Subheading = styled.h2`
   margin: ${styles.spacer.tiny} 0;
+`
+
+const Text = styled.p`
+  margin-bottom: ${styles.spacer.small};
+`
+
+const Link = styled.a`
+  &:visited {
+    color: ${styles.colour.black};
+  }
 `
